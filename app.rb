6c341da1000de1faa21ff17950aae9ebb44ace40
Chamberlain Churchill <cbchurchill2004@icloud.com>
2018-07-17 20:11:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/reversename/:name' do
+get '/reversename/:name' do
     params[:name].reverse
   end
 
@@ -10,6 +10,17 @@ get '/square/:number' do
    (num.to_i**2).to.s
 end
 
+get '/say/:number/:phrase' do
+  
+end
+
+get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+ end
+
+ get '/:operation/:number1/:number2' do
+  
+ end
 
 
 end
